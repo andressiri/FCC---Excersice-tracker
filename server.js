747fabe4b5602e0ee2e1ce96d34183150a7ad6cb
Andrés Siri <andres.siri@hotmail.com>
2021-10-10 14:30:59 -0300

    create databese's user schema and user model

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-app.use(cors())
-app.use(express.static('public'))
+// initial setup.
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// connect to MongoDB Atlas database.
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// set database's user Schema.
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true},
+  description: [String],
+  duration: Number,
+  date: Date
+});
+
+// set database's user model.
+const User = mongoose.model('User', userSchema);
+
+
